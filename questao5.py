6f03326fa140aa42591f0660f2ef78eb738ad334
PedroMesquitaIsidoro <83170114+PedroMesquitaIsidoro@users.noreply.github.com>
2022-01-18 16:30:47 -0300

    Create questao5.py

---
+++ questao5.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+recebidos = [160, 184, 241, 149, 180, 161, 132, 202, 160, 139, 149, 177]
+processados = [160, 184, 237, 148, 181, 150, 123, 156, 126, 104, 124, 140]
+mes = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago','Set','Out','Nov','Dez']
+
+x1 =  np.arange(len(recebidos))
+#print(x1)
+
+x2 = [x + 0.25 for x in x1]
+
+#print(x2)
+
+plt.bar(x1, recebidos, width=0.25, label = 'Recebidos', color = 'g')
+plt.bar(x2, processados, width=0.25, label = 'Processados', color = 'r')
+plt.xticks([x + 0.25 for x in range(len(recebidos))], mes)
+plt.title("Gráfico de recebidos e processados")
+plt.show()
