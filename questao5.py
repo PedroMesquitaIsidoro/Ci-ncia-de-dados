afc994944af1e024a84cf64d3d79e98c2558dbfe
PedroMesquitaIsidoro <83170114+PedroMesquitaIsidoro@users.noreply.github.com>
2022-01-18 16:47:27 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#questao 5 lista 02
-#Usando técnicas de Visualização de Dados, construa um gráfico a partir dos seguintes dados e mostre uma decisão que se pode tomar a partir dele
-
+#questão 5 lista 02
 recebidos = [160, 184, 241, 149, 180, 161, 132, 202, 160, 139, 149, 177]
 processados = [160, 184, 237, 148, 181, 150, 123, 156, 126, 104, 124, 140]
 mes = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago','Set','Out','Nov','Dez']
@@ -19,4 +17,7 @@ plt.bar(x1, recebidos, width=0.25, label = 'Recebidos', color = 'g')
 plt.bar(x2, processados, width=0.25, label = 'Processados', color = 'r')
 plt.xticks([x + 0.25 for x in range(len(recebidos))], mes)
 plt.title("Gráfico de recebidos e processados")
+plt.ylabel("Quantidade")
+plt.legend()
 plt.show()
+
